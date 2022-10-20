3a2dda40911b95351edbfe28381077262302675f
ihebbg <ibengara7@gmail.com>
2022-10-20 12:04:50 +0100

    third commit

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -5,18 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { PresentationComponent } from './presentation/presentation.component';
+import { ServicesComponent } from './_services/services.component';
+import { PartenaireComponent } from './partenaire/partenaire.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    PresentationComponent
+    PresentationComponent,
+    ServicesComponent,
+    PartenaireComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
